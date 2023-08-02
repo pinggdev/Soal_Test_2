8b7ed3523283f29285c71c082dfb82e5fab32250
Kevin Sinaga <kevinsinaga11.ks@gmail.com>
2023-08-03 03:01:14 +0700

    adding home section

--- src/layouts/Navbar/index.jsx
+++ src/layouts/Navbar/index.jsx
@@ -7,43 +7,43 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className={styles.navbarContainer}>
+    <div className="">
       <div
         className={styles.navbarBg}
         style={{ backgroundImage: `url(${navbarBg})` }}
       ></div>
-      <nav class="flex items-center justify-between flex-wrap bg-transparent p-6">
+      <nav className="flex items-center justify-between flex-wrap bg-transparent p-6">
         <Link to={""}>
-          <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <div className="flex items-center flex-shrink-0 text-white mr-6">
             <img src={logo} alt="logo" className="lg:w-24 w-12" />
-            <span class="font-bold lg:text-3xl text-lg ml-8">HOME</span>
+            <span className="font-bold lg:text-3xl text-lg ml-8">HOME</span>
           </div>
         </Link>
-        <div class="block lg:hidden"></div>
-        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-          <div class="text-sm lg:flex-grow"></div>
+        <div className="block lg:hidden"></div>
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div className="text-sm lg:flex-grow"></div>
           <div className="lg:mt-0 mt-5">
             <Link
               to={""}
-              class="items-center mt-4 lg:inline-block lg:mt-0  lg:text-black text-white font-bold lg:text-3xl text-lg  hover:text-blue-900 mr-12"
+              className="items-center mt-4 lg:inline-block lg:mt-0  lg:text-black text-white font-bold lg:text-3xl text-lg  hover:text-blue-900 mr-12"
             >
               ABOUT
             </Link>
             <Link
               to={""}
-              class=" mt-4 lg:inline-block lg:mt-0  lg:text-black text-white font-bold lg:text-3xl text-lg  hover:text-blue-900 mr-12"
+              className=" mt-4 lg:inline-block lg:mt-0  lg:text-black text-white font-bold lg:text-3xl text-lg  hover:text-blue-900 mr-12"
             >
               PRICING
             </Link>
             <Link
               to={""}
-              class=" mt-4 lg:inline-block lg:mt-0  lg:text-black text-white font-bold lg:text-3xl text-lg  hover:text-blue-900"
+              className=" mt-4 lg:inline-block lg:mt-0  lg:text-black text-white font-bold lg:text-3xl text-lg  hover:text-blue-900"
             >
               CONTACT
             </Link>
             <Link
               to={""}
-              class={`inline-block px-12 text-xl py-3 leading-none border rounded lg:text-blue-400 text-white border-white lg:border-blue-400 border-teal hover:border-transparent hover:text-white hover:bg-blue-900 mt-4 lg:mt-0 lg:ml-12 ml-0`}
+              className={`inline-block px-12 text-xl py-3 leading-none border rounded lg:text-blue-400 text-white border-white lg:border-blue-400 border-teal hover:border-transparent hover:text-white hover:bg-blue-900 mt-4 lg:mt-0 lg:ml-12 ml-0`}
             >
               LOGIN
             </Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import Navbar from "../layouts/Navbar";
+import sectionImg from "../../src/assets/section-home-1.svg";
+import waveImg from "../../src/assets/wave.png";
 
 function Home() {
   return (
     <div>
       <Navbar />
+      <div className="flex justify-center lg:mt-12 mt-24">
+        <h3 className="text-center w-96">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
+        </h3>
+      </div>
+      <div className="lg:grid grid-rows-2 grid-flow-col justify-between  lg:mt-40 mt-10">
+        <div className="justify-self-center text-center mx-10">
+          <p>
+            <i>"Lorem ipsum dolor sit, amet consectetur adipisicing"</i>
+          </p>
+        </div>
+        <div className="justify-self-center mx-10">
+          <img src={sectionImg} alt="sectionImg" className="w-64" />
+        </div>
+        <div className="row-span-2">
+          <img src={waveImg} alt="waveImg" className="" />
+        </div>
+      </div>
     </div>
   );
 }
